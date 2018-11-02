17a082b483a48ed8fda28e4d9697a48cb1321d35
francofriz <ffriz2010@gmail.com>
2018-11-02 17:27:10 +0100

    Added function to pass welcome activity to other

--- app/src/main/java/amplify/us/amplify/WelcomeActivity.java
+++ app/src/main/java/amplify/us/amplify/WelcomeActivity.java
@@ -1,13 +1,45 @@
 package amplify.us.amplify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private final int TRANSITION_TIME = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(TRANSITION_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    showNextScreen();
+                }
+            }
+
+        };
+        timer.start();
+
+    }
+
+    private void showNextScreen() {
+
+        Class activity = MainActivity.class;
+
+        startActivity(new Intent(WelcomeActivity.this, activity));
+        finish();
     }
 }
