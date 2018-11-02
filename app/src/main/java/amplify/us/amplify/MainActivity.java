e6f260cdb8d639e69d0edec25b6c869b6e8f9062
francofriz <ffriz2010@gmail.com>
2018-11-03 00:12:21 +0100

    - Styling the bottom navigation

--- app/src/main/java/amplify/us/amplify/MainActivity.java
+++ app/src/main/java/amplify/us/amplify/MainActivity.java
@@ -11,19 +11,20 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    private BottomNavigationView nav;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_discover:
                     mTextMessage.setText(R.string.title_discover);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_amplifySite:
                     mTextMessage.setText(R.string.title_amplify_site);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_profile:
                     mTextMessage.setText(R.string.title_profile);
                     return true;
             }
@@ -37,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        nav = (BottomNavigationView) findViewById(R.id.navigation);
+        nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
