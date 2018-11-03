db22ee60140e1381a2da637b9ed3a106d6916c6e
francofriz <ffriz2010@gmail.com>
2018-11-03 06:12:22 +0100

    - Added correctly recyclerView :DDD

--- app/src/main/java/amplify/us/amplify/DiscoverFragment.java
+++ app/src/main/java/amplify/us/amplify/DiscoverFragment.java
@@ -3,9 +3,17 @@ package amplify.us.amplify;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import amplify.us.amplify.adapters.EstablishmentAdapter;
+import amplify.us.amplify.entities.EstablishmentEntity;
 
 
 /**
@@ -13,6 +21,9 @@ import android.view.ViewGroup;
  */
 public class DiscoverFragment extends Fragment {
 
+    private RecyclerView rv_establishments_nearby;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -22,8 +33,29 @@ public class DiscoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        //simulate Services info
+        rv_establishments_nearby = (RecyclerView) rootView.findViewById(R.id.rv_establishments_nearby);
+        rv_establishments_nearby.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        rv_establishments_nearby.setLayoutManager(mLayoutManager);
+        mAdapter = new EstablishmentAdapter(dataSet());
+        rv_establishments_nearby.setAdapter(mAdapter);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        return rootView;
+    }
+
+    private ArrayList<EstablishmentEntity> dataSet(){
+        ArrayList<EstablishmentEntity> data = new ArrayList<>();
+        data.add(new EstablishmentEntity("Establishment 1","Info Establishment 1"));
+        data.add(new EstablishmentEntity("Establishment 2","Info Establishment 2"));
+        data.add(new EstablishmentEntity("Establishment 3","Info Establishment 3"));
+        data.add(new EstablishmentEntity("Establishment 4","Info Establishment 4"));
+        data.add(new EstablishmentEntity("Establishment 5","Info Establishment 5"));
+        return data;
     }
 
 }
--- app/src/main/java/amplify/us/amplify/adapters/EstablishmentAdapter.java
+++ app/src/main/java/amplify/us/amplify/adapters/EstablishmentAdapter.java
@@ -1,15 +1,44 @@
 package amplify.us.amplify.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import amplify.us.amplify.R;
 import amplify.us.amplify.entities.EstablishmentEntity;
 
-/**public class EstablishmentAdapter extends RecyclerView.Adapter<EstablishmentAdapter.EstablishmentViewHolder> {
+public class EstablishmentAdapter extends RecyclerView.Adapter<EstablishmentAdapter.EstablishmentViewHolder>{
     private ArrayList<EstablishmentEntity> data;
     public EstablishmentAdapter(ArrayList<EstablishmentEntity> data){
         this.data = data;
     }
-    @
-}*/
+    @Override
+    public EstablishmentViewHolder onCreateViewHolder (ViewGroup parent, int viewType){
+        return new EstablishmentViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.card_establishments_nearby,parent,false));
+    }
+    @Override
+    public void onBindViewHolder(EstablishmentViewHolder holder, int position){
+        EstablishmentEntity establishmentEntity = data.get(position);
+        holder.name_establishment.setText(establishmentEntity.getName());
+        holder.info_establishment.setText(establishmentEntity.getInfo());
+    }
+
+    @Override
+    public  int getItemCount(){
+        return data.size();
+    }
+
+    class EstablishmentViewHolder extends RecyclerView.ViewHolder{
+        TextView name_establishment;
+        TextView info_establishment;
+        public EstablishmentViewHolder (View itemView){
+            super(itemView);
+            name_establishment = (TextView) itemView.findViewById(R.id.name_establishment);
+            info_establishment = (TextView) itemView.findViewById(R.id.info_establishment);
+        }
+    }
+}
