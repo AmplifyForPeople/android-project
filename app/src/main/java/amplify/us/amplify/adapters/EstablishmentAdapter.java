01938c6a4a5807de2e84822bac2fa89076e36dcb
francofriz <ffriz2010@gmail.com>
2018-11-03 19:19:28 +0100

    - Added detail establishment and putExtra info

--- app/src/main/java/amplify/us/amplify/adapters/EstablishmentAdapter.java
+++ app/src/main/java/amplify/us/amplify/adapters/EstablishmentAdapter.java
@@ -1,13 +1,20 @@
 package amplify.us.amplify.adapters;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import amplify.us.amplify.DetailEstablishmentActivity;
+import amplify.us.amplify.DiscoverFragment;
+import amplify.us.amplify.MainActivity;
 import amplify.us.amplify.R;
 import amplify.us.amplify.entities.EstablishmentEntity;
 
@@ -25,6 +32,16 @@ public class EstablishmentAdapter extends RecyclerView.Adapter<EstablishmentAdap
         EstablishmentEntity establishmentEntity = data.get(position);
         holder.name_establishment.setText(establishmentEntity.getName());
         holder.info_establishment.setText(establishmentEntity.getInfo());
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),DetailEstablishmentActivity.class);
+                intent.putExtra("establishment_title",data.get(position).getName());
+                intent.putExtra("info_title",data.get(position).getInfo());
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -35,10 +52,12 @@ public class EstablishmentAdapter extends RecyclerView.Adapter<EstablishmentAdap
     class EstablishmentViewHolder extends RecyclerView.ViewHolder{
         TextView name_establishment;
         TextView info_establishment;
+        CardView parentLayout;
         public EstablishmentViewHolder (View itemView){
             super(itemView);
             name_establishment = (TextView) itemView.findViewById(R.id.name_establishment);
             info_establishment = (TextView) itemView.findViewById(R.id.info_establishment);
+            parentLayout = (CardView) itemView.findViewById(R.id.card_establishment);
         }
     }
 }
