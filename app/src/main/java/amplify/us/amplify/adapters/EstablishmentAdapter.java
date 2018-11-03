4c8018b2ee0c55d36ba521565e14e0ef39e11cd7
francofriz <ffriz2010@gmail.com>
2018-11-03 05:05:46 +0100

    - Created the establishmentAdapter,

---
+++ app/src/main/java/amplify/us/amplify/adapters/EstablishmentAdapter.java
@@ -0,0 +1,15 @@
+package amplify.us.amplify.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import amplify.us.amplify.entities.EstablishmentEntity;
+
+/**public class EstablishmentAdapter extends RecyclerView.Adapter<EstablishmentAdapter.EstablishmentViewHolder> {
+    private ArrayList<EstablishmentEntity> data;
+    public EstablishmentAdapter(ArrayList<EstablishmentEntity> data){
+        this.data = data;
+    }
+    @
+}*/
