33b44d518bb363e55a7d10fe8c8df26cab7df57f
francofriz <ffriz2010@gmail.com>
2018-11-04 11:31:38 +0100

    - Amplify Site -> similar song to song detail activity

--- app/src/main/java/amplify/us/amplify/AmplifySiteFragment.java
+++ app/src/main/java/amplify/us/amplify/AmplifySiteFragment.java
@@ -1,8 +1,10 @@
 package amplify.us.amplify;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,18 @@ public class AmplifySiteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_amplify_site, container, false);
+
+
+        //Similar song to -> song detail
+        CardView card_view = rootView.findViewById(R.id.similar_amp); // creating a CardView and assigning a value.
+
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),DetailSongActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
 
         return rootView;
     }
