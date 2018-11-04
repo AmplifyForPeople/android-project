8cacb565f3e12524bcf3bab60f0df1a7b17e368d
francofriz <ffriz2010@gmail.com>
2018-11-04 19:25:28 +0100

    - Done Edit Prof and add Change Password

--- app/src/main/java/amplify/us/amplify/EditProfileActivity.java
+++ app/src/main/java/amplify/us/amplify/EditProfileActivity.java
@@ -1,7 +1,13 @@
 package amplify.us.amplify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        this.set_spinner_data(R.id.set_self_data_sex, R.array.set_self_data_sexs);
+        this.set_spinner_data(R.id.set_self_data_location, R.array.set_self_data_locations);
+        this.save();
+        this.cancel();
+    }
+
+    private void set_spinner_data(int element_id, int array_id){
+        Spinner spinner = (Spinner) findViewById(element_id);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                array_id, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    private void save(){
+        Button safeBut = (Button) findViewById((R.id.save_but));
+        safeBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void cancel(){
+        Button cancelBut = (Button) findViewById((R.id.cancel_but));
+
+        cancelBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/amplify/us/amplify/ProfileFragment.java
+++ app/src/main/java/amplify/us/amplify/ProfileFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 /**
@@ -27,11 +28,25 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+
+        //Profile -> My Fav list Songs
         FrameLayout favLayout = (FrameLayout) rootView.findViewById(R.id.frameLayout_fav);
         favLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),FavouriteSongListActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        //Profile -> Edit Profile
+
+        TextView editProf = (TextView) rootView.findViewById((R.id.editProf));
+
+        editProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),EditProfileActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
