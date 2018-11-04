847b73a1dca52804a406dfc9ba22d90273bfcd7c
francofriz <ffriz2010@gmail.com>
2018-11-04 15:40:54 +0100

    - added FavSongList activity (time to develop)

---
+++ app/src/main/java/amplify/us/amplify/FavouriteSongListActivity.java
@@ -0,0 +1,13 @@
+package amplify.us.amplify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavouriteSongListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_song_list);
+    }
+}
