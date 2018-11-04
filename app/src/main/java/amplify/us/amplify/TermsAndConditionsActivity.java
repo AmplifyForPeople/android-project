e1e78652d6ba636201e919d1309947fc21335ad4
kevingarmill <kevingarmill@gmail.com>
2018-11-04 13:12:28 +0100

    terms and conditions

--- app/src/main/java/amplify/us/amplify/TermsAndConditionsActivity.java
+++ app/src/main/java/amplify/us/amplify/TermsAndConditionsActivity.java
@@ -1,26 +1,26 @@
 package amplify.us.amplify;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class TermsAndConditionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_terms_and_conditions);
 
-        Button button = (Button)findViewById(R.id.buttonToTermsAndConditions);
-        button.setOnClickListener(new View.OnClickListener() {
+        ImageView backArrow = (ImageView)findViewById(R.id.backToLogin);
+        backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(),TermsAndConditionsActivity.class);
+                Intent myIntent = new Intent(view.getContext(),WelcomeActivity.class);
                 startActivityForResult(myIntent,0);
-        }
+            }
         });
     }
 }
