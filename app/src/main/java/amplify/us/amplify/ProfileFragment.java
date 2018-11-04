1fa696a9e85c77ef45913241bd2134ad3cec8726
francofriz <ffriz2010@gmail.com>
2018-11-04 17:22:45 +0100

    - Created and done My Favourites songs (with and a listView Adapter)

--- app/src/main/java/amplify/us/amplify/ProfileFragment.java
+++ app/src/main/java/amplify/us/amplify/ProfileFragment.java
@@ -1,11 +1,13 @@
 package amplify.us.amplify;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
 /**
@@ -23,7 +25,22 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        FrameLayout favLayout = (FrameLayout) rootView.findViewById(R.id.frameLayout_fav);
+        favLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),FavouriteSongListActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+
+
+        return rootView;
+
     }
 
 }
